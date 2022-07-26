e46047f80527f50bb880d2f3efbf9db3edef4290
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2022-07-26 20:23:59 +0300

    main

--- src/homePage/homePageFirstSection.tsx
+++ src/homePage/homePageFirstSection.tsx
@@ -1,3 +1,19 @@
 export default function HomePageFirstSection(params: any) {
-  return <div></div>;
+  return (
+    <section>
+      <div>
+        <h1>
+          <span>test</span>
+          <br />
+          <span>test2</span>
+        </h1>
+        <div>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+            libero ea inventore explicabo similique eligendi
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
